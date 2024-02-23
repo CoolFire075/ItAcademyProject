30ab7658b43a37297c70539d48b110e149fca4a1
esenbekovnurgazy <esenbekovnurgazy.2008.kg@gmail.com>
2024-02-24 01:56:29 +0600

    my first commit

---
+++ src/codingbat/Warmup1/Makes10.java
@@ -0,0 +1,19 @@
+package codingbat.Warmup1;
+
+import java.util.Scanner;
+
+public class Makes10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input first number:");
+        int a = sc.nextInt();
+        System.out.println("Input first number:");
+        int b = sc.nextInt();
+        boolean isNumberEqually10 = makes10(a, b);
+        System.out.println(isNumberEqually10);
+    }
+    public static boolean makes10(int a, int b) {
+        if (a = 10 || b = 10 )
+            return
+    }
+}
