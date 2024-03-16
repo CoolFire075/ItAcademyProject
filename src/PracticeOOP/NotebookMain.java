06ab333f185bb201b33991897f7c61ba9fe07de1
esenbekovnurgazy <esenbekovnurgazy.2008.kg@gmail.com>
2024-03-16 14:04:50 +0600

    my first commit

--- src/PracticeOOP/NotebookMain.java
+++ src/PracticeOOP/NotebookMain.java
@@ -1,9 +1,11 @@
 package PracticeOOP;
 
+import java.util.Arrays;
+
 public class NotebookMain {
     public static void main(String[] args) {
 
-
+        char[] stickersArray = new char[]{'а', 'б', 'в', 'г', 'д', 'е'};
         Hdd hdd = new Hdd(13, 124);
         Hdd hdd2 = new Hdd(15, 136);
         Hdd hdd3 = new Hdd(17, 248);
@@ -13,32 +15,12 @@ public class NotebookMain {
         Ram ram = new Ram(18, "SDD");
         Ram ram2 = new Ram(22, "HDD");
         Ram[] ramArray = new Ram[]{ram, ram2};
-        Notebook notebook = new Notebook(hddArray, cpu, operatingSystem, ramArray);
+        Notebook notebook = new Notebook(hddArray, cpu, operatingSystem, ramArray, stickersArray);
 
         Cpu cpu2 = new Cpu("i9", 9, 20.5);
         notebook.setCpu(cpu2);
         OperatingSystem operatingSystem2 = new OperatingSystem("Ubuntu", 16.04);
         notebook.setOperatingSystem(operatingSystem2);
-        printNotebook(notebook);
-    }
-
-
-    public static void printNotebook(Notebook notebook) {
-        System.out.println("Notebook:"
-                + "\nCpu model: " + notebook.getCpu().getModel()
-                + "; core amount: " + notebook.getCpu().getCoreAmount()
-                + "; frequency: " + notebook.getCpu().getFrequency()
-                + "\nOperating version name: " + notebook.getOperatingSystem().getName()
-                + "; version: " + notebook.getOperatingSystem().getVersion());
-
-        for (int i = 0; i < notebook.getHddArray().length; i++) {
-            System.out.println("Hdd [" + i + "] size = " + notebook.getHddArray()[i].getSize()
-                    + "\nHdd [" + i + "] speed = " + notebook.getHddArray()[i].getSpeed());
-        }
-        for (int j = 0; j < notebook.getRamArray().length; j++) {
-            System.out.println("Ram [" + j + "] model = " + notebook.getRamArray()[j].getModel()
-                    + "\nRam [" + j + "] size = " + notebook.getRamArray()[j].getSize());
-        }
+        System.out.println(notebook);
     }
 }
-
